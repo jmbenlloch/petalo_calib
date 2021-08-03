def5144f0883ad6f755450c7b03a6cb9460a7dbf
jmbenlloch <info@jmbenlloch.net>
2021-08-03 17:25:16 +0200

    Add linear interpolation

--- petalo_calib/qdc_corrections.py
+++ petalo_calib/qdc_corrections.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy  as np
 
 from scipy.interpolate import interp1d
 
@@ -29,17 +30,24 @@ def compute_qdc_calibration_using_mode(fname_qdc):
 
     df_calib = pd.concat(dfs)
     df_calib.reset_index(drop=True, inplace=True)
+
+    # Replace first entry (3) with 0 to avoid out of range errors when interpolating (really) small windows.
+    df_calib.loc[df_calib.intg_w == 3, 'intg_w'] = 0
+    # Replace last entry (291) with 2000 to avoid out of range errors when interpolating (really) large windows.
+    df_calib.loc[df_calib.intg_w == 291, 'intg_w'] = 2000
+
     return df_calib
 
 
 def create_qdc_interpolator_df(fname_qdc_0, fname_qdc_2):
     df_qdc0 = pd.read_hdf(fname_qdc_0)
     df_qdc2 = pd.read_hdf(fname_qdc_2)
+    df_qdc = pd.concat([df_qdc0, df_qdc2]).reset_index()
     df_interpolators = df_qdc.groupby(['tofpet_id', 'channel_id', 'tac_id']).apply(lambda df: interp1d(df.intg_w, df.efine))
     return df_interpolators
 
 
 def compute_efine_correction_using_linear_interpolation(df, df_interpolators):
     df['correction']      = df.apply(lambda row: df_interpolators[row.tofpet_id, row.channel_id, row.tac_id](row.intg_w), axis=1)
-    df['efine_corrected'] = df.efine - df.correction
+    df['efine_corrected'] = (df.efine - df.correction).astype(np.float64) # For some reason df.correction is Object, not f64
     df.drop(columns=['correction'], inplace=True)
---
+++ petalo_calib/scripts/process_files_linear_interpolation.py
@@ -0,0 +1,60 @@
+from petalo_calib.tdc_corrections import correct_tfine_wrap_around
+from petalo_calib.qdc_corrections import correct_efine_wrap_around
+
+from petalo_calib.tdc_corrections import apply_tdc_correction
+from petalo_calib.qdc_corrections import compute_efine_correction_using_linear_interpolation
+from petalo_calib.qdc_corrections import create_qdc_interpolator_df
+
+from petalo_calib.tdc_corrections import compute_integration_window_size
+from petalo_calib.tdc_corrections import add_tcoarse_extended_to_df
+
+from petalo_calib.clustering import compute_evt_number_combined_with_cluster_id
+from petalo_calib.io         import compute_file_chunks_indices
+from petalo_calib.io         import write_corrected_df_daq
+
+import pandas as pd
+import sys
+
+
+def process_daq_df(df, df_tdc, df_qdc):
+    compute_integration_window_size(df)
+
+    correct_tfine_wrap_around(df)
+    correct_efine_wrap_around(df)
+
+    df = apply_tdc_correction(df, df_tdc)
+
+    compute_efine_correction_using_linear_interpolation(df, df_qdc)
+
+    add_tcoarse_extended_to_df(df)
+
+    df.drop(columns=['card_id', 'wordtype_id'], inplace=True)
+    compute_evt_number_combined_with_cluster_id(df)
+    return df
+
+
+def process_daq_file(filein, fileout, df_tdc, qf_qdc):
+    chunks = compute_file_chunks_indices(filein)
+    nchunks = chunks.shape[0]
+
+    for i in range(nchunks-1):
+        print("{}/{}".format(i, nchunks-2))
+        start = chunks[i]
+        end   = chunks[i+1]
+
+        df = pd.read_hdf(filein, 'data', start=start, stop=end+1)
+
+        df_corrected = process_daq_df(df, df_tdc, df_qdc)
+        write_corrected_df_daq(fileout, df_corrected, i, i>0)
+
+
+qdc0_fname = '/home/jmbenlloch/calibration/corrections/qdc_asic0_10772_linear_interpolation.h5'
+qdc2_fname = '/home/jmbenlloch/calibration/corrections/qdc_asic2_10746_linear_interpolation.h5'
+df_qdc     = create_qdc_interpolator_df(qdc0_fname, qdc2_fname)
+
+tfine_cal = '/home/jmbenlloch/calibration/corrections/scripts/asic0_tfine_cal.h5'
+df_tdc = pd.read_hdf(tfine_cal,key='tfine_cal')
+
+filein  = sys.argv[1]
+fileout = sys.argv[2]
+process_daq_file(filein, fileout, df_tdc, df_qdc)
